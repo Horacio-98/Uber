e29f4cc71c1df7ce108d7e011cb6cb612d1273ad
Horacio Granados <granadoshoracio2@gmail.com>
2023-03-26 17:16:20 -0600

    Conversion Fecha OIC

---
+++ JS/fecha.js
@@ -0,0 +1,11 @@
+function convertDate(dateString) {
+  const [date, time] = dateString.split(' ');
+  const [month, day, year] = date.split('-');
+  const [hour, minute, second] = time.split(':');
+
+  // const isoDate = '${year}-${month}-${day}T${hour}:${minute}:${second}Z';
+  const isoFecha = year+"-"+month+"-"+day+"T"+hour+":"+minute+":"+second+"Z";
+  var fecha = isoFecha; 
+  return fecha; 
+}
+
