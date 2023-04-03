169cbea1d67cfaf9a0e7495e420452450434484d
Horacio Granados <granadoshoracio2@gmail.com>
2023-04-03 02:57:48 -0600

    Update OIC files

--- JS/record.js
+++
@@ -1,5 +0,0 @@
-function formatRecord(num) {
-  formattedNum = num.toString().padStart(6, '0');
-  resultado = "SPA-" + formattedNum;
-  return resultado; 
-}
--- Python/main.py
+++ Python/main.py
@@ -6,6 +6,4 @@ from payment import payment
 from paypal import paypal
 
 if __name__ == "__main__" :
-    
-    paypal_1 = paypal('5','dawdawd')
-    print(paypal_1.info())
+    car_1 = car()
